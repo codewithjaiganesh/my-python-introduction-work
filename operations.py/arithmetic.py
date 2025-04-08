586ca02d07d35ca61270ad2181ef157a3b162197
codewithjaiganesh <jaiganeshreddy69@gmail.com>
2025-04-08 11:21:00 +0530

    first commit

---
+++ operations.py/arithmetic.py
@@ -0,0 +1,20 @@
+a = 3 
+b = 2
+z= a + b
+print(z)
+Y = a-b
+print(Y)
+x = a * b 
+print(x)
+U = a / b 
+print(U)
+# floor divsion 
+v = a//b 
+print(v)
+# modulo 
+T = a % b 
+print(T)
+# exponentiation 
+S = a ** b 
+print(S)
+
