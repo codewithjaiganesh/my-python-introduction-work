16c7f90fbbcebebfb91933942f7082907b63ca4e
codewithjaiganesh <jaiganeshreddy69@gmail.com>
2025-04-08 11:55:03 +0530

    click here

---
+++ operations.py/assignment.py
@@ -0,0 +1,48 @@
+# = assignment 
+A = "john"
+print(A)
+
+# += add and assign 
+a = 10 
+b = 12
+a += b
+print(a)
+
+# -= subtract and assign 
+d = 20
+e = 15 
+d -= e 
+print(d)
+
+# *= multiply and assign 
+t = 6 
+o = 3 
+t *= o
+print(t)
+
+# /= divide and assign 
+y = 100 
+u = 10 
+y /= u 
+print(y)
+
+# // floor an assign 
+k = 20 
+j = 5 
+k //= j 
+print(k)
+
+# %= modulo and assign 
+l = 100
+p = 900
+l %= p
+print(l)
+
+# **= Exponentiate and assign 
+h = 50
+f = 2
+h **= f
+print(h)
+
+
+
